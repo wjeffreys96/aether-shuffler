dad2529ead5380f6c5b8fd0fd9c3d3c7b16bed06
William <wjeffreys96@gmail.com>
2023-05-05 00:15:48 -0700

    GetData component created

---
+++ src/app/components/GetData.js
@@ -0,0 +1,16 @@
+export default function GetData(colorId, cardType, random_number) {
+    const params = {
+        "q": `f:commander ci:${colorId} t:${cardType} order:edhrec dir:asc`,
+        "page": `${random_number}`
+    }
+    const url = `https://api.scryfall.com/cards/search?${new URLSearchParams(params)}`
+
+    async function fetcher(url) {
+        const res = await fetch(url);
+        const data = await res.json();
+        return data;
+    }
+
+    const cards = fetcher(url);
+    return cards;
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,20 @@
-import Image from 'next/image'
+import GetData from "./components/GetData"
 
-export default function Home() {
+export default async function Home() {
+
+  const response = await GetData("b", "creature", 1);
+  const data = response.data;
+  let names = [];
+
+  data.forEach(obj => {
+    names.push(obj.name + " ");
+  });
+
   return (
-    <main>
+    <main className='w-full'>
+      <div className='flex flex-col w-full h-full m-5'>
+        <ul>{names.map((name) => (<li>{name}</li>))}</ul>
+      </div>
     </main>
   )
 }
