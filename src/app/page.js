4b7ba772e82105defe7631841d83c01a522df138
William <wjeffreys96@gmail.com>
2023-05-09 13:58:31 -0700

    bulkdata too slow. removed feature

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,11 @@
-export default async function Home() {
-  const params = {colorId: "b", type:"creature", page:1};
 
+
+export default function Home() {
   return (
     <main className="w-full">
       <div className="flex content-center justify-center p-3">
       </div>
-      <div className="flex flex-col w-full h-full m-5">
-      </div>
+      <div className="flex flex-col w-full h-full m-5"></div>
     </main>
   );
-}
+};
