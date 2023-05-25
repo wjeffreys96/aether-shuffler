5271ff24fcf42f462a4b901ed54f20c6db09e91e
William <wjeffreys96@gmail.com>
2023-05-24 20:56:23 -0700

    changed look of cardlist crudops buttons

--- src/app/components/CardLayout.js
+++ src/app/components/CardLayout.js
@@ -1,16 +1,28 @@
-"use client"
-import { useContext } from "react";
-import { AuthContext } from "../auth/AuthContext";
-
-export default function CardLayout({ name, id, imageUri }) {
-  const ctx = useContext(AuthContext);
-
-  const handleImgClick = () => {};
+export default function CardLayout({ name, id, imageUri, onClick, isOverlay, isFavorited, hideOverlay, AddToFavorites }) {
+  const handleOverlayClick = (event) => {
+    event.stopPropagation();
+    hideOverlay();
+  };
 
   return (
     <>
-      <div key={id} className="flex justify-center py-5">
-        <img className="cursor-pointer" onClick={handleImgClick} height="75%" width="75%" src={imageUri} alt={name} />
+      <div key={id} className="flex justify-center my-5">
+        <img className="cursor-pointer" onClick={onClick} height="75%" width="75%" src={imageUri} alt={name} />
+        {isOverlay && (
+          <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full" onClick={handleOverlayClick}>
+            <button
+              className={`w-24 h-24 flex items-center justify-center opacity-50 hover:opacity-75 rounded-full bg-black ${isFavorited ? 'bg-green-500' : ''}`}
+              onClick={(event) => {
+                event.stopPropagation();
+                AddToFavorites(name, id, imageUri);
+              }}
+            >
+              <span className="text-white">
+                {isFavorited ? '✔' : '+'}
+              </span>
+            </button>
+          </div>
+        )}
         <br />
         <hr />
       </div>
--- src/app/components/CardList.js
+++ src/app/components/CardList.js
@@ -1,4 +1,4 @@
-"use client";
+import React, { useState, useEffect } from "react";
 import CardLayout from "./CardLayout";
 import { useContext } from "react";
 import { getDatabase, ref, set } from "firebase/database";
@@ -6,42 +6,55 @@ import { AuthContext } from "../auth/AuthContext";
 
 export default function CardList({ cardData }) {
   const ctx = useContext(AuthContext);
+  const [favoritedCards, setFavoritedCards] = useState([]);
+  const [overlayCard, setOverlayCard] = useState(null);
+
+  useEffect(() => {
+    // reset the favoritedCards state when cardData changes
+    setFavoritedCards([]);
+    setOverlayCard(null);
+  }, [cardData]);
+
   function AddToFavorites(name, id, imageUri) {
     const db = getDatabase();
     const user = ctx.user;
     const uid = user.uid;
+
     set(ref(db, "users/" + uid + "/favorites/" + id), {
       name: name,
       id: id,
       imageUri: imageUri,
+    }).then(() => {
+      console.log("Added to favorites: ", name, id, imageUri);
+      setFavoritedCards((prevFavorites) => [...prevFavorites, id]);
     });
-    console.log("Added to favorites: ", name, id, imageUri);
   }
+
+  const hideOverlay = () => {
+    setOverlayCard(null);
+  };
 
   return (
     <div className="flex flex-col w-full h-full">
       <div className="">
-        <div className="h-auto flex justify-center flex-wrap px-3">
+        <div className="h-auto flex justify-center flex-wrap mx-3">
           {cardData.map((card) => {
+            const isFavorited = favoritedCards.includes(card.id);
+            const isOverlay = overlayCard === card.id;
+
             return (
-              <div key={card.name} className="flex flex-col ">
+              <div key={card.name} className="relative flex flex-col ">
                 <CardLayout
+                  AddToFavorites={AddToFavorites}
                   key={card.name}
                   name={card.name}
                   id={card.id}
                   imageUri={card.imageUri}
+                  onClick={() => setOverlayCard(card.id)}
+                  isOverlay={isOverlay}
+                  isFavorited={isFavorited}
+                  hideOverlay={hideOverlay}
                 />
-                <div className="flex justify-center">
-                  <button
-                    key={card.name}
-                    className="bg-blue-500 hover:bg-blue-700 w-1/3 mx-3 text-white text-sm font-bold rounded"
-                    onClick={() =>
-                      AddToFavorites(card.name, card.id, card.imageUri)
-                    }
-                  >
-                    + Add to Favorites
-                  </button>
-                </div>
               </div>
             );
           })}
@@ -50,3 +63,4 @@ export default function CardList({ cardData }) {
     </div>
   );
 }
+
